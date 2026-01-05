b48c4a80e1f52e8fbfc6a4aed045653476affd85
W1ck3dZA <quinten.rowland@gmail.com>
2026-01-05 13:39:38 +0200

    Add sample config file

---
+++ config-sample.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * iMobility SippySoft PHP Wrapper Credentials
+ *
+ * This script demonstrates how to use the SippySoftClient class.
+ * Make sure to update the credentials and host with your actual values.
+ */
+
+$username = '';
+$password = '';
+$host = '';
+
+?>
